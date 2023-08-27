668e26ec78153d6d4be877722888692cf23d8e9e
AyomideAdeyemi <129201767+AyomideAdeyemi@users.noreply.github.com>
2023-08-27 01:09:35 +0100

    Models Defined

--- src/backend/PharmacyInventory_Domain/Entities/User.cs
+++ src/backend/PharmacyInventory_Domain/Entities/User.cs
@@ -6,8 +6,7 @@ namespace PharmacyInventory_Domain.Entities
     public class User : IdentityUser
     {
         public string Id { get; set; }
-        public string FirstName { get; set; }
-        public string SecoundName { get; set; }
+        public string Name { get; set; }
         public Position Position { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime ModifiedAt { get; set; } = DateTime.UtcNow;
