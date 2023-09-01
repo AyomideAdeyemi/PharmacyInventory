42d16e7a1b2b611b9b420c6466ebea8cb47a8883
AyomideAdeyemi <129201767+AyomideAdeyemi@users.noreply.github.com>
2023-09-01 23:59:03 +0100

    More services regist and email notifications

---
+++ src/backend/PharmacyInventory_Domain/Dtos/Requests/ExpireNotificationRequestDto.cs
@@ -0,0 +1,17 @@
+﻿using PharmacyInventory_Shared.RequestParameter.ModelParameters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyInventory_Domain.Dtos.Requests
+{
+    public class ExpireNotificationRequestDto
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DrugRequestInputParameter Parameter { get; set; }
+
+    }
+}
