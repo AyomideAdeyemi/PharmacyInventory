5aa9e6ba60f8cb0616d61e9b77696c639da5f865
AyomideAdeyemi <129201767+AyomideAdeyemi@users.noreply.github.com>
2023-09-01 23:55:36 +0100

    services recorded

--- src/backend/PharmacyInventory_WebApi/Services/ServiceExtensions.cs
+++ src/backend/PharmacyInventory_WebApi/Services/ServiceExtensions.cs
@@ -1,11 +1,70 @@
-﻿using PharmacyInventory_Infrastructure.UnitOfWorkManager;
+﻿using PharmacyInventory_Application.Services.Implementations;
+using PharmacyInventory_Application.Services.Interfaces;
+using PharmacyInventory_Infrastructure.UnitOfWorkManager;
+using PharmacyInventory_Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using PharmacyInventory_Infrastructure.Persistence;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace PharmacyInventory_WebApi.Services
 {
     public static class ServiceExtensions
     {
-        public static void ConfigureIUnitOfWork(this IServiceCollection services) =>
-         services.AddScoped<IUnitOfWork, UnitOfWork>();
-        
+
+
+        public static void DependencyInjection(this IServiceCollection services)
+        {
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<ISupplierService, SupplierService>();
+            services.AddScoped<IBrandService, BrandService>();
+            services.AddScoped<IGenericNameService, GenericNameService>();
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+           // services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IUnitService, UnitService>();
+            services.AddScoped<IDrugService, DrugService>();
+        }
+
+        public static void ConfigureIdentity(this IServiceCollection services)
+        {
+            var builder = services.AddIdentity<User, IdentityRole>(o =>
+            {
+                o.Password.RequireDigit = true;
+                o.Password.RequireLowercase = false;
+                o.Password.RequireUppercase = false;
+                o.Password.RequireNonAlphanumeric = false;
+                o.Password.RequiredLength = 8;
+                o.User.RequireUniqueEmail = true;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+        }
+        public static void ConfigureJWT(this IServiceCollection services, IConfiguration
+configuration)
+        {
+            var jwtSettings = configuration.GetSection("JwtSettings");
+            var secretKey = Environment.GetEnvironmentVariable("SECRET");
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings["validIssuer"],
+                    ValidAudience = jwtSettings["validAudience"],
+                    IssuerSigningKey = new
+    SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
+                };
+            });
+        }
+
     }
 }
