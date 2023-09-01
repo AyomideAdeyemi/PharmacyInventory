785f811ba88d81d2d5cb3ffbc64fad066f6e733e
AyomideAdeyemi <129201767+AyomideAdeyemi@users.noreply.github.com>
2023-09-01 23:49:21 +0100

    Mapped my Classes together

---
+++ src/backend/PharmacyInventory_Application/Common/MapInitializers.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using PharmacyInventory_Domain.Dtos.Requests;
+using PharmacyInventory_Domain.Dtos.Responses;
+using PharmacyInventory_Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyInventory_Application.Common
+{
+    public class MapInitializers : Profile
+    {
+        public MapInitializers()
+        {
+            CreateMap<SupplierRequestDto, Supplier>();
+            CreateMap<Supplier, SupplierResponseDto>();
+
+            CreateMap<DrugRequestDto, Drug>();
+            CreateMap<Drug, DrugResponseDto>();
+
+            CreateMap<BrandRequestDto, Brand>();
+            CreateMap<Brand, BrandResponseDto>();
+
+            CreateMap<GenericNameRequestDto, GenericName>();
+            CreateMap<GenericName, GenericNameResponseDto>();
+
+            CreateMap<UserRequestDto, User>();
+
+
+        }
+
+    }
+}
