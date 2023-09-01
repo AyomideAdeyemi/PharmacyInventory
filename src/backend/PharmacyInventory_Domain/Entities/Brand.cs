fa40abc5e83618a13f7b91a7d68e76b49a284053
AyomideAdeyemi <129201767+AyomideAdeyemi@users.noreply.github.com>
2023-09-01 23:38:31 +0100

    Brand and User Claseses Updated

--- src/backend/PharmacyInventory_Domain/Entities/Brand.cs
+++ src/backend/PharmacyInventory_Domain/Entities/Brand.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PharmacyInventory_Domain.Entities
+﻿namespace PharmacyInventory_Domain.Entities
 {
     public class Brand : AuditableBaseEntity
     {
--- src/backend/PharmacyInventory_Domain/Entities/User.cs
+++ src/backend/PharmacyInventory_Domain/Entities/User.cs
@@ -4,8 +4,7 @@ using PharmacyInventory_Domain.Enum;
 namespace PharmacyInventory_Domain.Entities
 {
     public class User : IdentityUser
-    {
-        public string Id { get; set; }
+    {     
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public Position Position { get; set; }
