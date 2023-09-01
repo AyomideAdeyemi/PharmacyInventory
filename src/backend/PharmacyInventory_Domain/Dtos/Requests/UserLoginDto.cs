53dd5b5340bc370a099fc9a4cd6dbaf8c4e1cce5
AyomideAdeyemi <129201767+AyomideAdeyemi@users.noreply.github.com>
2023-09-01 23:39:45 +0100

    User login and roles added

--- src/backend/PharmacyInventory_Domain/Dtos/Requests/UserLoginDto.cs
+++ src/backend/PharmacyInventory_Domain/Dtos/Requests/UserLoginDto.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ContactBook_Domain.Dtos.Requests
+namespace PharmacyInventory_Domain.Dtos.Requests
 {
     public class UserLoginDto
     {
---
+++ src/backend/PharmacyInventory_Infrastructure/Repository/RoleConfigurations.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace PharmacyInventory_Infrastructure.Repository
+{
+    public class RoleConfigurations : IEntityTypeConfiguration<IdentityRole>
+    {
+        public void Configure(EntityTypeBuilder<IdentityRole> builder)
+        {
+
+
+            builder.HasData(
+ new IdentityRole
+ {
+     Name = "Admin",
+     NormalizedName = "ADMIN"
+ },
+ new IdentityRole
+ {
+     Name = "User",
+     NormalizedName = "USER"
+ }
+ );
+        }
+    }
+}
+
