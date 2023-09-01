26886ee4d0fb5ec69dda43b4569767d552c85475
AyomideAdeyemi <129201767+AyomideAdeyemi@users.noreply.github.com>
2023-09-01 23:45:01 +0100

    Added Pagination

---
+++ src/backend/PharmacyInventory_Shared/RequestParameter/Common/PagedList.cs
@@ -0,0 +1,28 @@
+﻿using PharmacyInventory_Shared.RequestParameter.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyInventory_Shared.RequestParameter.Common
+{
+    public class PagedList<T> : List<T>
+    {
+        public MetaData MetaData { get; set; }
+
+        public PagedList(List<T> items, int count, int pageNumber, int pageSize)
+        {
+            MetaData = new()
+            {
+                TotalCount = count,
+                PageSize = pageSize,
+                CurrentPage = pageNumber,
+                TotalPages = (int) Math.Ceiling( count / (double)pageSize)
+
+            };
+            AddRange(items);
+        }
+    }
+}
+ 
