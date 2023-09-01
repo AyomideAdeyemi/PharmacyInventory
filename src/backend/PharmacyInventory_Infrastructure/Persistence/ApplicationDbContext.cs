c8238759f23457285b6d96c0d7dfedeb8bd294ec
AyomideAdeyemi <129201767+AyomideAdeyemi@users.noreply.github.com>
2023-09-01 23:31:38 +0100

    Added Identity Dbcontext

--- src/backend/PharmacyInventory_Infrastructure/Persistence/ApplicationDbContext.cs
+++ src/backend/PharmacyInventory_Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,14 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using PharmacyInventory_Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using PharmacyInventory_Infrastructure.Repository;
 
 namespace PharmacyInventory_Infrastructure.Persistence
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<User>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
@@ -17,13 +14,14 @@ namespace PharmacyInventory_Infrastructure.Persistence
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            
+            modelBuilder.ApplyConfiguration(new RoleConfigurations());
+
+            base.OnModelCreating(modelBuilder);
         }
         public DbSet<Drug> drug { get; set; }
         public DbSet<Brand> brand { get; set; }
         public DbSet<Unit> unit { get; set; }
         public DbSet<Supplier> supplier { get; set; }
-        public DbSet<User> user { get; set; }
         public DbSet<GenericName> genericName { get; set; }
     }
 }
