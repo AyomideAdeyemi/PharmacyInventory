85cf8cb7ad64c36c3eae60a962245a5252f9429f
AyomideAdeyemi <129201767+AyomideAdeyemi@users.noreply.github.com>
2024-02-06 14:54:04 +0100

    Updated

--- src/backend/PharmacyInventory_WebApi/Program.cs
+++ src/backend/PharmacyInventory_WebApi/Program.cs
@@ -32,11 +32,14 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+app.UseSwagger();
+  app.UseSwaggerUI();
+
 app.UseCors("CorsPolicy");
 app.UseSwaggerUI(s =>
 {
